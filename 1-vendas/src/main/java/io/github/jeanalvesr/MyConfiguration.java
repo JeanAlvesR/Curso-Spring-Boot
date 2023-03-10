4658a7aeac285b5b319dffc4fc7688e21c67bd82
Jean <engjeanr@gmail.com>
2023-03-10 13:14:23 -0300

    Configurations and Bean

---
+++ 1-vendas/src/main/java/io/github/jeanalvesr/MyConfiguration.java
@@ -0,0 +1,11 @@
+package io.github.jeanalvesr;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class MyConfiguration {
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sistema de Vendas";
+    }
+}
--- 1-vendas/src/main/java/io/github/jeanalvesr/VendasApplication.java
+++ 1-vendas/src/main/java/io/github/jeanalvesr/VendasApplication.java
@@ -1,5 +1,7 @@
 package io.github.jeanalvesr;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
+
     @GetMapping("/hello")
     public String helloWorld(){
-        return "Hello World";
+        return applicationName;
     }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
