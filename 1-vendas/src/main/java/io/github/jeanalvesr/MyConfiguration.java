c66ccd938576ece1b8c4b607d85618c3d2a04537
Jean <engjeanr@gmail.com>
2023-03-10 15:11:06 -0300

    multiplos ambientes e profiles

--- 1-vendas/src/main/java/io/github/jeanalvesr/MyConfiguration.java
+++ 1-vendas/src/main/java/io/github/jeanalvesr/MyConfiguration.java
@@ -1,11 +1,22 @@
 package io.github.jeanalvesr;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 @Configuration
+@Profile("development")
 public class MyConfiguration {
     /*@Bean(name = "applicationName")
     public String applicationName(){
         return "Sistema de Vendas";
     }*/
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 }
