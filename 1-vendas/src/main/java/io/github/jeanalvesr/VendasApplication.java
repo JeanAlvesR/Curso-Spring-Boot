a8196103e79a28fe3a8b69a4ace5b739c856c0a3
Jean <engjeanr@gmail.com>
2023-03-10 12:52:58 -0300

    Hello Wold com Spring boot e RestController

---
+++ 1-vendas/src/main/java/io/github/jeanalvesr/VendasApplication.java
@@ -0,0 +1,18 @@
+package io.github.jeanalvesr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
