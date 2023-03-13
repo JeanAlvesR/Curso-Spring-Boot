5d9279d0e4e7c4e76e8af944f400a8805b32448b
Jean <engjeanr@gmail.com>
2023-03-13 19:48:25 -0300

    deletar, atualizar, buscar por nome -> adicionados

--- Vendas-Spring-Data/src/main/java/io/github/jeanalvesr/domain/repositorio/Clientes.java
+++ Vendas-Spring-Data/src/main/java/io/github/jeanalvesr/domain/repositorio/Clientes.java
@@ -1,6 +1,7 @@
 package io.github.jeanalvesr.domain.repositorio;
 
 import io.github.jeanalvesr.domain.entity.Cliente;
+import org.h2.result.Row;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -13,18 +14,45 @@ import java.util.List;
 @Repository
 public class Clientes {
 
-    private static String INSERT = "insert into cliente (nome) values (?) ";
+    private static String INSERT = "INSERT INTO CLIENTE (NOME) VALUES (?) ";
+    private static String UPDATE = "UPDATE CLIENTE SET NOME =? WHERE ID = ?";
     private static String SELECT_ALL = "SELECT * FROM CLIENTE";
+    private static String DELETE = "DELETE FROM CLIENTE WHERE ID = ?";
 
     @Autowired //Já vem configurado com base no properties para se conectar ao banco.
     private JdbcTemplate jdbcTemplate;
 
-    public Cliente salvar(Cliente cliente){
+    public Cliente salvar(Cliente cliente) {
         jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
         return cliente;
     }
 
-    public List<Cliente> obterTodos(){
-        return jdbcTemplate.query(SELECT_ALL, (rs, rowNum) -> new Cliente(rs.getInt("id"), rs.getString("nome")));
+    public Cliente atualizar(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(), cliente.getId()});
+        return cliente;
+    }
+
+    public List<Cliente> buscarPorNome(String nome) {
+        return jdbcTemplate.query(SELECT_ALL.concat(" where nome like  '%"+nome+"%'") , obterClienteMapper());
+    }
+
+    public void deletar(Cliente cliente) {
+        jdbcTemplate.update(DELETE, new Object[]{cliente.getId()});
+    }
+
+    public List<Cliente> obterTodos() {
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+    }
+
+    private RowMapper<Cliente> obterClienteMapper() {
+
+        return (rs, rowNum)->{return new Cliente(rs.getInt("id"), rs.getString("nome"));};
+//        new RowMapper<Cliente>() {
+//            @Override
+//            public Cliente mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                return new Cliente(rs.getInt("id"), rs.getString("nome"));
+//            };
+//
+//        };
     }
 }
