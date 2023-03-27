5e19ef828b3d8e086124c8f969e392a9f7e0a50b
Jean <engjeanr@gmail.com>
2023-03-27 16:22:03 -0300

    Utilizando padrão DTO

---
+++ Vendas-JPA-Rest-Refatorado/src/main/java/io/github/jeanalvesr/rest/dto/ItemPedidoDTO.java
@@ -0,0 +1,6 @@
+package io.github.jeanalvesr.rest.dto;
+
+public class ItemPedidoDTO {
+    private Integer produto;
+    private Integer quantidade;
+}
---
+++ Vendas-JPA-Rest-Refatorado/src/main/java/io/github/jeanalvesr/rest/dto/PedidoDTO.java
@@ -0,0 +1,11 @@
+package io.github.jeanalvesr.rest.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class PedidoDTO {
+    private Integer cliente;
+    private BigDecimal total;
+
+    private List<ItemPedidoDTO> itens;
+}
